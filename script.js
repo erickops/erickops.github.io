95c167ec2c5fa7bc66b8f881596d70c0c579e02b
Ericko Putra S <100887897+erickops@users.noreply.github.com>
2023-04-16 23:47:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-function typeEffect(element, speed) {
-  var text = element.innerHTML;
-  element.innerHTML = "";
-  
-  var i = 0;
-  var timer = setInterval(function() {
-    if (i < text.length) {
-      element.append(text.charAt(i));
-      i++;
-    } else {
-      clearInterval(timer);
-    }
-  }, speed);
+// import { gsap } from "https://cdn.skypack.dev/gsap";
+let open = false;
+
+const trigger = document.querySelector(".header__trigger");
+if (trigger) {
+  const headerNav = document.querySelector(".header__nav");
+  trigger.addEventListener("click", () => {
+    trigger.classList.toggle("open");
+    headerNav.classList.toggle("open");
+    const amount = open ? 0 : 100;
+    gsap.
+    timeline({}).
+    to(headerNav, 0.4, {
+      "--panel-bottom-1": amount,
+      ease: "Power1.easeOut" }).
+
+    to(
+    headerNav,
+    0.4,
+    {
+      "--panel-bottom-2": amount,
+      ease: "Power1.easeOut" },
+
+    0.1).
+
+    to(
+    headerNav,
+    0.4,
+    {
+      "--panel-bottom-3": amount,
+      ease: "Power1.easeOut" },
+
+    0.2).
+
+    to(
+    headerNav,
+    0.4,
+    {
+      "--panel-bottom-4": amount,
+      ease: "Power1.easeOut" },
+
+    0.3);
+
+    open = !open;
+  });
 }
-
-
-// application
-var speed = 75;
-var h1 = document.querySelector('h1');
-var p = document.querySelector('p');
-var delay = h1.innerHTML.length * speed + speed;
-
-// type affect to header
-typeEffect(h1, speed);
-
-
-// type affect to body
-setTimeout(function(){
-  p.style.display = "inline-block";
-  typeEffect(p, speed);
-}, delay);
